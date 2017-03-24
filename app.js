c7f0972a3ae041917a3c17369b97dfd321e0454b
Pedro Roldan <pedro.roldan@gmail.com>
2017-03-24 03:18:54 +0000

    Initial

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var tools = require('./server/initServer');
 var serverDateStart, serverDateNow, serverDateEnd;
 var SOCKET_LIST = {}; // lista de ligacoes
-var PLAYERS_LIST = {}; // lista de jogadores
 var playerCount = 0;
 
 // 40ms = 1000/25
@@ -15,17 +14,51 @@ var CYCLE_TIME = 1000/25;      // tempo em milisegundos para cada ciclo de loop
  * @returns {{name: *, id: *, number: string, x: number, y: number, local: number}}
  * @constructor
  */
-var Player = function (id,name) {
+var Player = function (id) {
     var self = {
-        name : name,
+        name : '',
         id : id,
         number : "" + Math.floor(10 * Math.random()),
         x : 0,
         y : 0,
-        local : 0
-    }
+        local : ""
+    };
+    Player.list[id] = self;
     return self;
 }
+
+var USERS = {
+    //username:password
+    "viper":"123",
+    "slyer":"123",
+    "bob":"1",
+    "obo":"1"
+};
+
+Player.list = {};
+Player.onConnect = function(socket,data){
+    var player = Player(socket.id);
+    player.name = data.username;
+    player.local = "40.0#175.0";
+};
+
+Player.onDisconnect = function(socket){
+    delete Player.list[socket.id];
+};
+
+Player.update = function(){
+    var pack = [];
+    for(var i in Player.list){
+        var player = Player.list[i];
+        //player.update();
+        pack.push({
+            id : player.id,
+            name : player.name,
+            local : player.local
+        });
+    }
+    return pack;
+};
 
 // Setup do servidor
 var app = tools.initServer();
@@ -33,7 +66,6 @@ var serv  = require("http").Server(app);
 serv.listen(2000); // liga o servidor em determinada porta
 
 serverDateNow=serverDateStart = new Date();
-
 console.log(serverDateStart + ' ->World@War Public Server pré-alpha Started...');
 
 var io  = require('socket.io') (serv,{});
@@ -44,12 +76,37 @@ io.sockets.on('connection', function (socket) {
 
     SOCKET_LIST[socket.id] = socket; // guarda ligacao na lista
 
+    socket.on('signIn',function(data){
+        isValidPassword(data,function(res){
+            if(res){
+                Player.onConnect(socket,data);
+                socket.emit('signInResponse',{success:true});
+
+                console.log("Player: " + data.username + " added!");
+            } else {
+                socket.emit('signInResponse',{success:false});
+            }
+        });
+    });
+
+    socket.on('signUp',function(data){
+        isUsernameTaken(data,function(res){
+            if(res){
+                socket.emit('signUpResponse',{success:false});
+            } else {
+                addUser(data,function(){
+                    socket.emit('signUpResponse',{success:true});
+                });
+            }
+        });
+    });
+
     /**
      * Metodo automatico que valida o disconect do socket pelo cliente
      */
     socket.on('disconnect', function () { // automatico nao necessita de mensagem do cliente
         delete SOCKET_LIST[socket.id]; // elimina a socket
-        delete PLAYERS_LIST[socket.id]; // elimina o player
+        Player.onDisconnect(socket); // elimina o player
 
         if (playerCount != 0) {
             playerCount = playerCount -1;
@@ -58,16 +115,8 @@ io.sockets.on('connection', function (socket) {
         console.log('player deleted' + ' id: ' + socket.id);
     })
 
+    /*
     socket.on("newPlayer", function (data) {
-        var playerExists = false;
-
-        for (var i in PLAYERS_LIST){
-            var player = PLAYERS_LIST[i];
-            if ( player.name == data.name) {
-                playerExists = true;
-            }
-        }
-
         if (!playerExists) {
             var player = Player(socket.id); // cria novo player
             player.name = data.name;
@@ -86,8 +135,8 @@ io.sockets.on('connection', function (socket) {
                     break;
             }
 
-            PLAYERS_LIST[socket.id] = player;
-            playerCount++;
+            //PLAYERS_LIST[socket.id] = player;
+            //playerCount++;
 
             socket.emit('newPlayerResponse',{
                 login       : true,
@@ -109,24 +158,40 @@ io.sockets.on('connection', function (socket) {
 
             socket.emit('playersList',pack);
 
-            console.log("Player: " + player.name + " added!");
-        } else {
-            socket.emit('newPlayerResponse',{
-                login : false
-            });
-
-            //console.log("Player: " + player.name + " exists!");
         }
 
     });
-
+*/
     console.log('new socket connection' + ' id: ' + socket.id + " ip: " + socket.ip);
 
 });
 
+
+
+
+
+var isValidPassword = function(data,cb){
+    setTimeout(function(){
+        cb(USERS[data.username] === data.password);
+    },10);
+};
+
+var isUsernameTaken = function(data,cb){
+    setTimeout(function(){
+        cb(USERS[data.username]);
+    },10);
+};
+
+var addUser = function(data,cb){
+    setTimeout(function(){
+        USERS[data.username] = data.password;
+        cb();
+    },10);
+};
+
 var add_minutes =  function (dt, minutes) {
     return new Date(dt.getTime() + minutes*60000);
-}
+};
 
 /**
  * Ciclo de loop
@@ -134,24 +199,17 @@ var add_minutes =  function (dt, minutes) {
 setInterval(function () {
 
     //serverDateNow = add_minutes(serverDateNow,1);
-        var pack = [];
+    var pack = {
+        player:Player.update()
+    };
 
-        for (var i in PLAYERS_LIST) {
-            var player = PLAYERS_LIST[i];
-            pack.push({
-                id : player.id,
-                name : player.name,
-                local : player.local
-            });
-        }
-
-        for (var i in SOCKET_LIST){
-            var socket = SOCKET_LIST[i];
-            socket.emit('serverStatus',{
-                playerCount : playerCount,
-                timestamp : new Date()
-            });
-            socket.emit('playersList',pack);
-        }
+    for (var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.emit('serverStatus',{
+            playerCount : playerCount,
+            timestamp : new Date()
+        });
+        socket.emit('playersList',pack);
+    }
 }, CYCLE_TIME);
 
